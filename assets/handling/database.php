0fe797513f1a3f3369cb0146fea9365faca8b142
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-10-05 00:18:38 -0400

    fixed?

--- assets/handling/database.php
+++ assets/handling/database.php
@@ -1,13 +1,13 @@
 <?php
-	define('DB_HOST', 'teassr-teassr.f.aivencloud.com');
-    	define('DB_USER', 'avnadmin');
-    	define('DB_PASS', 'AVNS_lynYZp2r1ezjPYQuoNT');
-    	define('DB_NAME', 'defaultdb');
+define('DB_HOST', 'teassr-teassr.f.aivencloud.com');
+define('DB_USER', 'avnadmin');
+define('DB_PASS', 'AVNS_lynYZp2r1ezjPYQuoNT');
+define('DB_NAME', 'defaultdb');
 
-	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-	if($conn->connect_error){
-		die('Connection failed: ' . $conn->connect_error);
-	}
-	
+if ($conn->connect_error) {
+	die('Connection failed: ' . $conn->connect_error);
+}
+
 ?>
