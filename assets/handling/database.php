dfb0336dbf5f00da015c0fb7e7194180bef7a3ef
senup1 <120296876+senup1@users.noreply.github.com>
2024-10-05 00:23:52 -0400

    omg no way

--- assets/handling/database.php
+++ assets/handling/database.php
@@ -3,6 +3,7 @@ define('DB_HOST', 'teassr-teassr.f.aivencloud.com');
 define('DB_USER', 'avnadmin');
 define('DB_PASS', 'AVNS_lynYZp2r1ezjPYQuoNT');
 define('DB_NAME', 'defaultdb');
+//e
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
