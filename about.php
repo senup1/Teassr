43ae6b4af3ec3a6b1166fa03ee798c8a02f86c98
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-10-06 19:44:54 -0400

    about page update

--- about.php
+++ about.php
@@ -7,11 +7,30 @@
     <section id="aboutPage">
         <div class="container">
             <div class="APItem">
-                <p class="APTitle"> WHO WE ARE: </p>
-                <p class="APBody">
+                <p class="APTitle"> WHAT IS TEASSR?: </p>
+                <p class="APBody" id="Body1">
                     Welcome to Teassr—a dynamic social media platform designed to foster connections and
                     creativity among users. Here, you can share your thoughts, engage with friends, and cultivate a
-                    vibrant online community. <br /> <br /> Have fun, and Teass On!!
+                    vibrant online community.
+                </p>
+            </div>
+            <div class="APItem">
+                <p class="APTitle"> OUR MISSION: </p>
+                <p class="APBody" id="Body1">
+                    At Teassr, we believe in the power of connection. Our mission is to provide a space
+                    where users can express themselves, share their experiences, and connect with others who share their
+                    interests. We aim to create a user-friendly environment that encourages interaction and
+                    collaboration.
+                </p>
+            </div>
+            <div class="APItem">
+                <p class="APTitle"> WHO ARE WE? </p>
+                <p class="APBody" id="Body2">
+                    As a web development project, Teassr showcases our technical skills and passion for creating
+                    engaging user experiences. From responsive design to intuitive navigation, every aspect of the
+                    platform has been crafted to enhance usability and interaction.
+                    Thank you for visiting Teassr. We're excited to share this journey with you as we connect, share,
+                    and grow together in our vibrant community! <br /> <br /> Have fun, and Teass On!!
                 </p>
             </div>
         </div>
