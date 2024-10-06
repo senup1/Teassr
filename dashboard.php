1ec4cec1510ed119053dd074b737778399f46ed7
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-10-06 18:01:21 -0400

    added ppl to dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,180 +1,180 @@
 <?php include 'assets/inc/header.php' ?>
 
 <title>Dashboard</title>
-	</head>
+</head>
 
-	<body>
-		<nav id="dashNav">
-			<div class="container">
-				<div>
-					<button onclick="menuActions()">Menu</button>
+<body>
+	<nav id="dashNav">
+		<div class="container">
+			<div>
+				<button onclick="menuActions()">Menu</button>
+			</div>
+			<div id="DNItems">
+				<div class=DNItem>
+					<button>temp</button>
 				</div>
-				<div id="DNItems">
-					<div class=DNItem>
-						<button>temp</button>
-					</div>
-					<div class=DNItem>
-						<button>temp</button>
-					</div>
-					<div class=DNItem>
-						<button>temp</button>
-					</div>
-					<div class=DNItem>
-						<button>temp</button>
-					</div>
-					<div class=DNItem>
-						<button>temp</button>
-					</div>
-					<div class=DNItem>
-						<button>temp</button>
-					</div>
+				<div class=DNItem>
+					<button>temp</button>
+				</div>
+				<div class=DNItem>
+					<button>temp</button>
+				</div>
+				<div class=DNItem>
+					<button>temp</button>
+				</div>
+				<div class=DNItem>
+					<button>temp</button>
+				</div>
+				<div class=DNItem>
+					<button>temp</button>
 				</div>
 			</div>
-		</nav>
+		</div>
+	</nav>
 
-		<section id="dashBody">
-			<div class="container">			
-				<div class="DBItem"></div>
-				<div class="DBItem">
-					<div id="postSection">
-						<ul class="container">
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-							<li class="postItem">
-								<a href="#">
-									<img class="postImg" src="assets/imgs/goldfish.jpg">
-									<p class="postTitle">asdf</p>
-								</a>
-							</li>
-						</ul>
-					</div>
+	<section id="dashBody">
+		<div class="container">
+			<div class="DBItem"></div>
+			<div class="DBItem">
+				<div id="postSection">
+					<ul class="container">
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl1.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl2.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl3.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl4.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl5.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl6.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl7.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl8.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl9.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl10.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl11.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl12.png">
+								<p class="postTitle">asdf</p>
+							</a>
+						</li>
+					</ul>
 				</div>
-				<div class="DBItem">
-					<div id="suggestSection">
-						<p id="suggestSectionTitle">Suggested Accounts: </p>
-						<ul class="container">
+			</div>
+			<div class="DBItem">
+				<div id="suggestSection">
+					<p id="suggestSectionTitle">Suggested Accounts: </p>
+					<ul class="container">
 						<li class="suggestItem">
-								<a href="#">
-									<div class="suggestSubItem">
-										<img class="suggestImg" src="assets/imgs/goldfish.jpg">
-									</div>
-									<div class="suggestSubItem">
-										<p>user</p>
-									</div>
-									<div class="suggestSubItem">
-										<button>follow</button>
-									</div>
-								</a>
-							</li>
-							<li class="suggestItem">
-								<a href="#">
-									<div class="suggestSubItem">
-										<img class="suggestImg" src="assets/imgs/goldfish.jpg">
-									</div>
-									<div class="suggestSubItem">
-										<p>user</p>
-									</div>
-									<div class="suggestSubItem">
-										<button>follow</button>
-									</div>
-								</a>
-							</li>
-							<li class="suggestItem">
-								<a href="#">
-									<div class="suggestSubItem">
-										<img class="suggestImg" src="assets/imgs/goldfish.jpg">
-									</div>
-									<div class="suggestSubItem">
-										<p>user</p>
-									</div>
-									<div class="suggestSubItem">
-										<button>follow</button>
-									</div>
-								</a>
-							</li>
-							<li class="suggestItem">
-								<a href="#">
-									<div class="suggestSubItem">
-										<img class="suggestImg" src="assets/imgs/goldfish.jpg">
-									</div>
-									<div class="suggestSubItem">
-										<p>user</p>
-									</div>
-									<div class="suggestSubItem">
-										<button>follow</button>
-									</div>
-								</a>
-							</li>
-						</ul>
-					</div>
+							<a href="#">
+								<div class="suggestSubItem">
+									<img class="suggestImg" src="assets/imgs/ppl/ppl2.png">
+								</div>
+								<div class="suggestSubItem">
+									<p>user</p>
+								</div>
+								<div class="suggestSubItem">
+									<button>follow</button>
+								</div>
+							</a>
+						</li>
+						<li class="suggestItem">
+							<a href="#">
+								<div class="suggestSubItem">
+									<img class="suggestImg" src="assets/imgs/ppl/ppl2.png">
+								</div>
+								<div class="suggestSubItem">
+									<p>user</p>
+								</div>
+								<div class="suggestSubItem">
+									<button>follow</button>
+								</div>
+							</a>
+						</li>
+						<li class="suggestItem">
+							<a href="#">
+								<div class="suggestSubItem">
+									<img class="suggestImg" src="assets/imgs/ppl/ppl2.png">
+								</div>
+								<div class="suggestSubItem">
+									<p>user</p>
+								</div>
+								<div class="suggestSubItem">
+									<button>follow</button>
+								</div>
+							</a>
+						</li>
+						<li class="suggestItem">
+							<a href="#">
+								<div class="suggestSubItem">
+									<img class="suggestImg" src="assets/imgs/ppl/ppl2.png">
+								</div>
+								<div class="suggestSubItem">
+									<p>user</p>
+								</div>
+								<div class="suggestSubItem">
+									<button>follow</button>
+								</div>
+							</a>
+						</li>
+					</ul>
 				</div>
-				<div class="DBItem"></div>
 			</div>
-		</section>
+			<div class="DBItem"></div>
+		</div>
+	</section>
 
 
-<?php include 'assets/inc/footer.php' ?>
+	<?php include 'assets/inc/footer.php' ?>
