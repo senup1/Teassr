cf48137f0100f4355cc3df6f141a40d6ef7f8bf3
senup1 <120296876+senup1@users.noreply.github.com>
2024-10-06 18:03:36 -0400

    changes to dash css and contact us form

--- contactUs.php
+++ contactUs.php
@@ -4,19 +4,20 @@
 	</head>
     <body>
         <section id="contactPage">
-            <div class="container">
-                <div class="CPItem">
-                    <p>Contact Us</p>
+            <p id="contactTitle">Contact Us</p>
+            <form action="handing/sendEmail.php" method="POST" class="container">
+                <div>
+                    <label for="name">Email:</label>
+                    <input type="email" name="email" required>  
                 </div>
-                <div class="CPItem">
-                    <form action="handing/sendEmail.php" method="POST">
-                        <label for="name">Email:</label>
-                        <input type="email" name="email" required>
-                        <label for="name">Message:</label>
-                        <textarea id="message"  name="message" rows="5" required></textarea>
-                    </form>
+                <div>
+                    <label for="name">Message:</label>
+                    <textarea id="message"  name="message" rows="5" required></textarea>
                 </div>
-            </div>
+                <div>
+                    <button type="submit">Send</button>
+                </div>
+            </form>
         </section>
 
         
