7ca9e5c99b87d20b4e22fb56605d7cd43ae008f5
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-10-06 18:49:49 -0400

    updated about page

--- about.php
+++ about.php
@@ -1,18 +1,20 @@
 <?php include 'assets/inc/header.php' ?>
 
 <title>About Teassr</title>
-	</head>
-    <body>
-        <section id="aboutPage">
-            <div class="container">
-                <div class="APItem">
-                    <p class="APTitle"> WHO WE ARE: </p>
-                    <p class="APBody">
-                        Teassr is a social media platform that allows users to develop authentic connections with others in a safe and simple environment.
-                    </p>
-                </div>
-            </div>
-        </section>
+</head>
 
-        
-<?php include 'assets/inc/footer.php' ?>
+<body>
+    <section id="aboutPage">
+        <div class="container">
+            <div class="APItem">
+                <p class="APTitle"> WHO WE ARE: </p>
+                <p class="APBody">
+                    Teassr is a social media platform that allows users create an account, post to your followers, and
+                    connected with your friends! All in a simple and safe envirnment. Have fun, and Teass On!!
+                </p>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include 'assets/inc/footer.php' ?>
