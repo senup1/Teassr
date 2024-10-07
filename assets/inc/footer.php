5d300437dfd9c1d6c6f35bea73dc5fef6581b4d8
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-10-06 20:30:29 -0400

    created Terms and Privacy Page

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,26 +1,27 @@
-		<footer id="mainFooter">
-			<div class="container">
-				<div class="footerItem">
-					<p>&copy; 2024 Teassr</p>
-				</div>
-				<div class="footerItem">
-					<a href="../../../../about.php">About</a>
-				</div>
-				<div class="footerItem">
-					<a href="../../../../faq.php">FAQ</a>
-				</div>
-				<div class="footerItem">
-					<a href="../../../../contactUs.php">Contact Us</a>
-				</div>
-				<div class="footerItem">
-					<a href="#">Terms & Privacy</a>
-				</div>
-				<div class="footerItem">
-					<a href="#">DMCA</a>
-				</div>
-			</div>
-		</footer>
+<footer id="mainFooter">
+	<div class="container">
+		<div class="footerItem">
+			<p>&copy; 2024 Teassr</p>
+		</div>
+		<div class="footerItem">
+			<a href="../../../../about.php">About</a>
+		</div>
+		<div class="footerItem">
+			<a href="../../../../faq.php">FAQ</a>
+		</div>
+		<div class="footerItem">
+			<a href="../../../../contactUs.php">Contact Us</a>
+		</div>
+		<div class="footerItem">
+			<a href="../../../../termsAndPrivacy.php">Terms & Privacy</a>
+		</div>
+		<div class="footerItem">
+			<a href="#">DMCA</a>
+		</div>
+	</div>
+</footer>
 
-		<script src="assets/scripts/main.js"></script>
-	</body>
+<script src="assets/scripts/main.js"></script>
+</body>
+
 </html>
---
+++ termsandprivacy.php
@@ -0,0 +1,16 @@
+<?php include 'assets/inc/header.php' ?>
+
+<title>Terms and Privacy</title>
+</head>
+
+<body>
+    <section id="termsAndPrivacy">
+        <div class="container">
+            <div class="TPItem">
+                <p class="TPTitle">temp</p>
+                <p class="TPBody">temp</p>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'assets/inc/footer.php' ?>
