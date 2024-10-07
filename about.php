c4374ddde34b10c4a02c2fb63ca09bbf0675014f
senup1 <120296876+senup1@users.noreply.github.com>
2024-10-06 20:15:22 -0400

    simplified about page content

--- about.php
+++ about.php
@@ -9,28 +9,19 @@
             <div class="APItem">
                 <p class="APTitle"> WHAT IS TEASSR?: </p>
                 <p class="APBody">
-                    Welcome to Teassr—a dynamic social media platform designed to foster connections and
-                    creativity among users. Here, you can share your thoughts, engage with friends, and cultivate a
-                    vibrant online community.
+                    Welcome to Teassr—a dynamic social media platform designed to foster connections, share your thoughts, and cultivate a vibrant online community.
                 </p>
             </div>
             <div class="APItem">
                 <p class="APTitle"> OUR MISSION: </p>
                 <p class="APBody">
-                    At Teassr, we believe in the power of connection. Our mission is to provide a space
-                    where users can express themselves, share their experiences, and connect with others who share their
-                    interests. We aim to create a user-friendly environment that encourages interaction and
-                    collaboration.
+                    At Teassr, our mission is to create a user-friendly environment where users can express themselves, share their experiences, and connect with others who share their interests.
                 </p>
             </div>
             <div class="APItem">
                 <p class="APTitle"> WHO ARE WE? </p>
                 <p class="APBody">
-                    As a web development project, Teassr showcases our technical skills and passion for creating
-                    engaging user experiences. From responsive design to intuitive navigation, every aspect of the
-                    platform has been crafted to enhance usability and interaction.
-                    Thank you for visiting Teassr. We're excited to share this journey with you as we connect, share,
-                    and grow together in our vibrant community! <br /> <br /> Have fun, and Teass On!!
+                    As a web development project, Teassr showcases our technical skills and passion for creating engaging user experiences. From responsive design to intuitive navigation, every aspect of the platform has been crafted to enhance usability and interaction. We're excited to share this journey with you as we connect, share, and grow together in our vibrant community! <br /> <br /> Have fun, and Teass On!!
                 </p>
             </div>
             <div class="APItem">
