ab011a5fa40d2c61e59defb2f199164153170a59
senup1 <120296876+senup1@users.noreply.github.com>
2024-10-12 22:07:22 -0400

    updated images and titles on dashboard

--- dashboard.php
+++ dashboard.php
@@ -21,193 +21,133 @@
 					<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl1.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">beatiful view in the mountains</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl2.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">when mom says there's goldfish at home</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl3.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">chilling in some rice fields</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl4.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">enjoying the view today, climbing tomorrow</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl5.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">spent some time thinking by the river</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl6.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">family photo in our backyard</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl7.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">got a new haircut</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl8.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">me when midterms are coming up</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl9.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">relaxing in the park</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl10.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">just finished my morning run</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl11.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">enjoying coffee by the beach</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl12.png">
-								<p class="postTitle">asddf</p>
+								<p class="postTitle">my trip to the mountains this weekend</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl3.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">farming from minecraft irl</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl14.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">trying new styles</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl15.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">morning person for life</p>
+							</a>
+						</li>
+						<li class="postItem">
+							<a href="#">
+								<img class="postImg" src="assets/imgs/ppl/ppl18.png">
+								<p class="postTitle">all alone on the bleachers</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl16.png">
-								<p class="postTitle">asddf</p>
+								<p class="postTitle">found some houses in the mountains</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl1.png">
-								<p class="postTitle">asdf</p>
+								<img class="postImg" src="assets/imgs/ppl/ppl17.png">
+								<p class="postTitle">got my blue contacts</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl2.png">
-								<p class="postTitle">asdf</p>
+								<img class="postImg" src="assets/imgs/ppl/ppl19.png">
+								<p class="postTitle">tried dying my hair</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl3.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl4.png">
-								<p class="postTitle">asdf</p>
+								<img class="postImg" src="assets/imgs/ppl/ppl20.png">
+								<p class="postTitle">my favorite from today's photoshoot</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl5.png">
-								<p class="postTitle">asdf</p>
+								<p class="postTitle">spent some time thinking by the river</p>
 							</a>
 						</li>
 						<li class="postItem">
 							<a href="#">
 								<img class="postImg" src="assets/imgs/ppl/ppl6.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl7.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl8.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl9.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl10.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl11.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl12.png">
-								<p class="postTitle">asddf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl3.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl14.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl15.png">
-								<p class="postTitle">asdf</p>
-							</a>
-						</li>
-						<li class="postItem">
-							<a href="#">
-								<img class="postImg" src="assets/imgs/ppl/ppl16.png">
-								<p class="postTitle">asddf</p>
+								<p class="postTitle">family photo in our backyard</p>
 							</a>
 						</li>
 					</ul>
