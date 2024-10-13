a35550dbab69dbc98fed5f88331295dcc1ab0353
senup1 <120296876+senup1@users.noreply.github.com>
2024-10-13 12:19:06 -0400

    completed errorPage (for 404 etc.)

---
+++ error.php
@@ -0,0 +1,28 @@
+<?php include 'assets/inc/header.php' ?>
+
+<title>Teassr Error</title>
+</head>
+
+<body>
+
+    <?php include 'assets/inc/navbar.php' ?>
+
+	<div class="spaceMaker"></div>
+
+    <section id="errorPage">
+        <div class="container">
+            <div class="errorItem">
+                <p id="errorTitle">Sorry</p>
+            </div>
+            <div class="errorItem">
+                <p id="errorInfo">This page does not exist</p>
+            </div>
+            <div class="errorItem">
+                <a href="../../../../dashboard.php">Go back to Teassr</a>
+            </div>
+            
+        </div>
+    </section>
+
+
+    <?php include 'assets/inc/footer.php' ?>
