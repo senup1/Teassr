1b8cb071fb86711266f14ad1ff0bcbcf6e971b7d
senup1 <120296876+senup1@users.noreply.github.com>
2024-10-13 22:20:36 -0400

    slight progress on message page outline

--- messages.php
+++ messages.php
@@ -10,7 +10,60 @@
 	<div class="spaceMaker"></div>
 
     <section id="messages">
-        
+        <div class="container">
+            <div class="messagesItem">
+                <ul id="friendList">
+                    <li class="friendItem">
+                        <div class="friendSubItem">
+                            <img class="messageItemPFP" src="assets/imgs/pfps/pfp1.png">
+                        </div>
+                        <div class="friendSubItem">
+                            <p class="messageItemUsername">goldenApple43</p>
+                            <p class="lastMessage">OMG I JUST FOUND A NOTCH APPLE!</p>
+                        </div>
+                    </li>
+                    <li class="friendItem">
+                        <div class="friendSubItem">
+                            <img class="messageItemPFP" src="assets/imgs/pfps/pfp2.png">
+                        </div>
+                        <div class="friendSubItem">
+                            <p class="messageItemUsername">GoatGIRL1999</p>
+                            <p class="lastMessage">cows aren't even that great</p>
+                        </div>
+                    </li>
+                    <li class="friendItem">
+                        <div class="friendSubItem">
+                            <img class="messageItemPFP" src="assets/imgs/pfps/pfp3.png">
+                        </div>
+                        <div class="friendSubItem">
+                            <p class="messageItemUsername">JimmyNeutron</p>
+                            <p class="lastMessage">we're going to the moon tonight</p>
+                        </div>
+                    </li>
+                    <li class="friendItem">
+                        <div class="friendSubItem">
+                            <img class="messageItemPFP" src="assets/imgs/pfps/pfp4.png">
+                        </div>
+                        <div class="friendSubItem">
+                            <p class="messageItemUsername">PowerpuffCheerio</p>
+                            <p class="lastMessage">I couldn't have imagined such a betrayal</p>
+                        </div>
+                    </li>
+                    <li class="friendItem">
+                        <div class="friendSubItem">
+                            <img class="messageItemPFP" src="assets/imgs/pfps/pfp5.png">
+                        </div>
+                        <div class="friendSubItem">
+                            <p class="messageItemUsername">NetheriteGOD69</p>
+                            <p class="lastMessage">lol i just got full Netherite.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="messagesItem">
+                i donno yet
+            </div>
+        </div>
     </section>
 
 
